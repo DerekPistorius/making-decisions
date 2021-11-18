83a85bfa4af388e12375f4fb010c7dd42decafe2
Derek <DerekPistorius@devmounta.in>
2021-11-17 20:24:38 -0800

    problem 10 is solved

--- labMine.js
+++ labMine.js
@@ -167,7 +167,8 @@ console.log(changeMyMind)
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-
+changeMyMind = !changeMyMind
+console.log(changeMyMind)
 
 
 ////////// ADVANCED PROBLEMS //////////
